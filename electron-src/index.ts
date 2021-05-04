29e375573f218dc6fff784dac2b3a61ee54a0102
kaoru.ohtaka <ockacru+git@gmail.com>
2021-05-05 00:36:18 +0900

    Can setup eslint for electron

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -36,6 +36,7 @@ app.on('window-all-closed', app.quit)
 
 // listen the channel `message` and resend the received message to the renderer process
 ipcMain.on('message', (event: IpcMainEvent, message: any) => {
+  // eslint-disable-next-line no-console
   console.log(message)
   setTimeout(() => event.sender.send('message', 'hi from electron'), 500)
 })
