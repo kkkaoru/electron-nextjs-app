6d1042c7917ef971aae0c733dafb8afc179c70a7
kaoru.ohtaka <ockacru+git@gmail.com>
2021-05-05 00:38:35 +0900

    Fix eslintrc files

--- electron-src/.eslintrc.js
+++ electron-src/.eslintrc.js
@@ -1,6 +1,6 @@
 /** @type import('eslint').Linter.BaseConfig */
 module.exports = {
-  extends: ['airbnb-typescript', 'prettier'],
+  extends: ['airbnb-typescript/base', 'prettier'],
   parserOptions: {
     project: `${__dirname}/tsconfig.json`,
   },
@@ -12,16 +12,8 @@ module.exports = {
   rules: {
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/require-default-props': 'off',
     'max-lines': 'error',
     'max-depth': 'error',
     'max-lines-per-function': 'error',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        aspects: ['preferButton'],
-      },
-    ],
   },
 };
